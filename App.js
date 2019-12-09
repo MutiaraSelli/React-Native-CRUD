722cccd49dfaa5790aba404e2225fe6478e0e55c
MutiaraSelli <selliaprilinamutiara@gmail.com>
2019-12-09 22:09:10 +0700

    https://github.com/MutiaraSelli/React-Native-CRUD

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
-/**
+import React, {Component} from 'react';
+import Rootstack from './src/Navigator/Rootstack.js'
 
-* Sample React Native App
-
-* https://github.com/facebook/react-native
-
-* @flow
-
-*/
-import React, { Component } from 'react';
-import {
-StyleSheet,
-View,
-StatusBar
-} from 'react-native';
-import Routes from './src/Routes';
-export default class App extends Component   {
-render() {
-return (
-<View style={styles.container}>
-<StatusBar
-backgroundColor="#1c313a"
-barStyle="light-content"
-/>
-<Routes/>
-</View>
-);
+export default class App extends Component {
+  render() {
+    return (
+      <Rootstack />
+    );
+  }
 }
-}
-const styles = StyleSheet.create({
-container : {
-flex: 1,
-}
-});
